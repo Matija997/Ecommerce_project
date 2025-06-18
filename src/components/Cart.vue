e781afd0b2d0efe7f20279be5c538ba7493d86ef
Matija997 <matijamarinkovic@gmail.com>
2025-06-18 17:13:41 +0200

    Initial commit

---
+++ src/components/Cart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="border: 1px solid #aaa; padding: 10px; margin-bottom: 20px;">
+    <h2>Shopping Cart</h2>
+    <div v-if="cartItems.length === 0">Cart is empty.</div>
+    <ul v-else>
+      <li v-for="item in cartItems" :key="item.id" style="margin-bottom: 5px;">
+        {{ item.name }} - ${{ item.price }} x {{ item.quantity }} = ${{ item.price * item.quantity }}
+        <button @click="$emit('remove', item.id)" style="margin-left: 10px;">Remove</button>
+      </li>
+    </ul>
+    <div v-if="cartItems.length > 0" style="margin-top: 10px; font-weight: bold;">
+      Total: ${{ total }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cartItems: Array
+  },
+  computed: {
+    total() {
+      return this.cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0)
+    }
+  }
+}
+</script>
---
+++ src/components/ProductItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="display: flex; justify-content: space-between; align-items: center;">
+    <div>
+      <strong>{{ product.name }}</strong> - ${{ product.price }}
+    </div>
+    <button @click="$emit('add-to-cart')">Add to cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object
+  }
+}
+</script>
